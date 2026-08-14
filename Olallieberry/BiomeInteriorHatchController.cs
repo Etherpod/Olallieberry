4947c4029e0af988430bec274f2769faf33ee0a9
Noah Pilarski <megapiggy@outerwildsmods.com>
2026-08-13 23:47:25 -0400

    Biome Interior Hatch Controller

---
+++ Olallieberry/BiomeInteriorHatchController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace Olallieberry;
+
+public class BiomeInteriorHatchController : MonoBehaviour
+{
+    [Header("Hatch")]
+    public float openAngle = -85f;
+    public float rotationSpeed = 180f;
+
+    private Quaternion _closedRotation;
+    private Quaternion _openRotation;
+    private bool _open;
+
+    public void Awake()
+    {
+        _closedRotation = transform.localRotation;
+        _openRotation = _closedRotation * Quaternion.Euler(0f, openAngle, 0f);
+    }
+
+    public void Update()
+    {
+        Quaternion targetRotation = _open ? _openRotation : _closedRotation;
+
+        transform.localRotation = Quaternion.RotateTowards(
+            transform.localRotation,
+            targetRotation,
+            rotationSpeed * Time.deltaTime
+        );
+    }
+
+    public void Open()
+    {
+        _open = true;
+    }
+
+    public void Close()
+    {
+        _open = false;
+    }
+
+    public void SetOpen(bool open)
+    {
+        _open = open;
+    }
+
+    public void Toggle()
+    {
+        _open = !_open;
+    }
+}
