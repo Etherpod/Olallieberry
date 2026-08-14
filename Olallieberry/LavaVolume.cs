b720343a28b53c4942ffa2f2d6e308d15ee8e01c
Noah Pilarski <megapiggy@outerwildsmods.com>
2026-08-14 09:33:46 -0400

    lava volume

---
+++ Olallieberry/LavaVolume.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Olallieberry;
+
+/// <summary>
+/// Kills the player or retrieves the probe when they enter lava.
+/// </summary>
+public class LavaVolume : EffectVolume
+{
+    public void OnValidate()
+    {
+        _triggerVolume = gameObject.GetAddComponent<OWTriggerVolume>();
+        _triggerVolume.Reset();
+    }
+
+    public override void OnEffectVolumeEnter(GameObject hitObj)
+    {
+        if (hitObj.CompareTag("PlayerDetector"))
+        {
+            Locator.GetDeathManager().KillPlayer(DeathType.Lava);
+        }
+        else if (hitObj.CompareTag("ProbeDetector"))
+        {
+            hitObj.GetAttachedOWRigidbody(false)
+                .GetRequiredComponent<SurveyorProbe>()
+                .ExternalRetrieve(false);
+        }
+    }
+
+    public override void OnEffectVolumeExit(GameObject hitObj)
+    {
+    }
+}
