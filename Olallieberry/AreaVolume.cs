55ce0b2798c1d1913ba7ca791af4d7e95f0207f4
Noah Pilarski <megapiggy@outerwildsmods.com>
2026-08-14 06:06:05 -0400

    area volume

---
+++ Olallieberry/AreaVolume.cs
@@ -0,0 +1,55 @@
+﻿using OWML.Common;
+using UnityEngine;
+
+namespace Olallieberry;
+
+/// <summary>
+/// Identifies the major areas of the station.
+/// </summary>
+public enum StationArea
+{
+    None = -1,
+    Entrance,
+    Hub,
+    Basalt,
+    Caves,
+    Spikes,
+    Beach,
+    Sphere,
+    Shortcut
+}
+
+/// <summary>
+/// Logs whenever an object enters/exits a station area.
+/// </summary>
+public class AreaVolume : EffectVolume
+{
+    /// <summary>
+    /// The area represented by this volume.
+    /// </summary>
+    [Header("Area")]
+    [Tooltip("The area represented by this volume.")]
+    public StationArea area;
+
+    public void OnValidate()
+    {
+        _triggerVolume = gameObject.GetAddComponent<OWTriggerVolume>();
+        _triggerVolume.Reset();
+    }
+
+    public override void OnEffectVolumeEnter(GameObject hitObj)
+    {
+        Olallieberry.Instance.ModHelper.Console.WriteLine(
+            $"[AreaVolume] {hitObj.name} entered {area}.",
+            MessageType.Info
+        );
+    }
+
+    public override void OnEffectVolumeExit(GameObject hitObj)
+    {
+        Olallieberry.Instance.ModHelper.Console.WriteLine(
+            $"[AreaVolume] {hitObj.name} exited {area}.",
+            MessageType.Info
+        );
+    }
+}
