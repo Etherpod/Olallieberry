6c95b7a76f568bf3a374e40164b76e0be38c96da
ditzy <ditzy.zy@pm.me>
2026-08-15 20:45:47 -0500

    add orb gear spin controller

---
+++ Olallieberry/OrbSpinController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Olallieberry;
+
+public class OrbSpinController : MonoBehaviour
+{
+	[SerializeField] private float spinRate;
+
+	private float spinRateMultiplier = 0;
+
+	private void OnEnable()
+	{
+		spinRateMultiplier = 360 / spinRate;
+	}
+
+	private void FixedUpdate()
+	{
+		if (OWTime.IsPaused()) return;
+		
+		transform.Rotate(Vector3.up, Time.fixedDeltaTime * spinRateMultiplier, Space.Self);
+	}
+}
